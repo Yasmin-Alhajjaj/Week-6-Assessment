40859413200e45fa82080d22693655b0be2b5716
Yasmin-Alhajjaj <yasmin.alhajjaj@gmail.com>
2019-07-21 10:24:20 +0300

    finish assmint

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 console.log('GOOD LUCK 👩‍💻 👨‍💻')
 /* Q1:
-Usind For Each
-Create a function called addKeyPowerIndex
-that takes an array of objects as a parameter
-and return this array with add a key to each object
-that is the (index) of this object in this array
-and the value will be the (power 2) of the index
+
 
 var arrOfObj1 = [
   { a: 12 },
@@ -35,14 +30,41 @@ Ex: addKeyPowerIndex(arrOfObj2)
   {0: 0, a: 'cat'},
   {1: 1, b: 'dog'},
   {2: 4, c: 'duck'}
-]   
+]
+
+Usind For Each
+Create a function called addKeyPowerIndex
+that takes an array of objects as a parameter
+and return this array with add a key to each object
+that is the (index) of this object in this array
+and the value will be the (power) of the index
 */
 
-function addKeyPowerIndex() {
-  // WRITE YOUR CODE UNDER THIS LINE  
+function addKeyPowerIndex(array) {
+  // WRITE YOUR CODE UNDER THIS LINE 
+  var result=[];
+  var index=0;
+  array.forEach((element,i)=> {
+  element[index]=index*index 
+    index++
+  });
+  
 
+return array;
 }
+console.log( addKeyPowerIndex([
+  { a: 'cat' },
+  { b: 'dog' },
+  { c: 'duck' }
+]))
 
+console.log( addKeyPowerIndex([
+  { a: 12 },
+  { b: 5 },
+  { c: 16 },
+  { d: 4 },
+  { e: 3 }
+]))
 
 
 
@@ -63,32 +85,31 @@ Ex: decreseBy(arrOfnum1,10)
 Ex: decreseBy(arrOfNum1,-6)
 => [83, 11, 39]
 */
-
-function decreseBy() {
+var arrOfNum1 = [77,5,33]
+function decreseBy(array,num) {
   // WRITE YOUR CODE UNDER THIS LINE 
+var result=[];
+result= array.map(element=>element-num)
 
+return result;
 }
-
-
+console.log( decreseBy(arrOfNum1,10))
+console.log( decreseBy(arrOfNum1,-6))
 
 
 
 
 /* Q3:
-Using Filter
-Create a function called nameLongerThan
-that takes an array of objects and number as a parameter
-and return a new array with only the object has a value inside 
-the key name longer than this number
 
-var arrOfObj3 = [
+
+var arrOfObj4 = [
   { name: "alex" },
   { name: "mercer"},
   { name: "alice" },
   { name: "zaheer"},
   { name: "elizabeth"}
 ]
-Ex: nameLongerThan(arrOfObj3,4)
+Ex: nameLongerThan(arrOfObj4,4)
 =>[
   { name: "mercer"},
   { name: "alice" },
@@ -96,16 +117,33 @@ Ex: nameLongerThan(arrOfObj3,4)
   { name: "elizabeth"}
 ]
 
-Ex: nameLongerThan(arrOfObj3,6)
+Ex: nameLongerThan(arrOfObj4,6)
 =>[
   { name: "elizabeth"}
 ]
+
+Using Filter
+Create a function called nameLongerThan
+that takes an array of objects and number as a parameter
+and return a new array with only the object has a value inside 
+the key name longer than this number
 */
+var arrOfObj4 = [
+  { name: "alex" },
+  { name: "mercer"},
+  { name: "alice" },
+  { name: "zaheer"},
+  { name: "elizabeth"}]
 
-function nameLongerThan() {
-  // WRITE YOUR CODE UNDER THIS LINE         
+function nameLongerThan(array,num) {
+  // WRITE YOUR CODE UNDER THIS LINE
+  var result=[];
+result= array.filter(element=>(element.name).length>num)
+  
+return result;
 }
-
+console.log(nameLongerThan(arrOfObj4,4))
+console.log(nameLongerThan(arrOfObj4,6))
 
 
 
@@ -132,10 +170,45 @@ Ex: avgLength(arrOfObj4,"name")
 Ex: avgLength(arrOfObj4,"food")
 => 6.8
 */
+var arrOfObj4 = [
+  { name: "alex", food: "fried chiken" },
+  { name: "mercer", food: "pizaa" },
+  { name: "alice", food: "burger" },
+  { name: "zaheer", food: "hot dog" },
+  { name: "elizabeth", food: "eggs" }
+]
 
-function avgLength() {
+/* function avgLength(array,str) {
   // WRITE YOUR CODE UNDER THIS LINE         
+var result;
+result= array.reduce(function(total,cur){
+total=total.length+(cur.str).length
 
+return total
+},(array[0].str.length))
+
+return result;
 }
+console.log(avgLength(arrOfObj4,"name"))
+// Good luck :) */
 
-// Good luck :)
+function avgLength(array,str) {
+  var result;
+  var i=0
+  result= array.reduce(function(total,cur){
+    total=total+cur[str].length
+   i=i+1
+
+
+    return total
+  
+  },0)
+
+
+
+
+  return result/i;
+}
+console.log(avgLength(arrOfObj4,"name"))
+console.log(avgLength(arrOfObj4,"food"))
+
